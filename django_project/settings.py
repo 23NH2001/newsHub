18431d66f3f6f45f332050118019120c86f7b4ab
Neveel Hingu <99134102+23NH2001@users.noreply.github.com>
2023-09-15 17:54:42 -0400

    add Bootstrap styling and password reset function.

--- django_project/settings.py
+++ django_project/settings.py
@@ -138,3 +138,6 @@ LOGOUT_REDIRECT_URL = "homePage"
 # 3rd party
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+# Email
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
